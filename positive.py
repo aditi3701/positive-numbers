9ed04a2fffdc43874003a017e4c9d4551bce9919
aditi3701 <80256674+aditi3701@users.noreply.github.com>
2021-03-19 21:05:13 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,4 @@
+Inputlist1 = [12,-7,5,64,-14]
+for num in Inputlist1:
+    if num >= 0:
+       print(num, end = " ")
---
+++ positive1.py
@@ -0,0 +1,4 @@
+element = [12,14,-95,3]
+print("Input:list2 ",element)
+new_list = list(filter(lambda a: a >0, element))
+print("Output: ",new_list)
